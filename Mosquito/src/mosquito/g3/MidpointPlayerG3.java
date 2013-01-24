21cb7635eefce9a6b99202b07bbbe3e134f51094
Dan Klein <kosmotrainer@gmail.com>
2013-01-24 04:20:03 -0500

    Nightly Commit 24 Jan 2013
    Lots of progress with MidpointPLayer. For testing purposes, it places
    all "critical points" in the game board as lights. All I need to do now
    is work on the spacing, redundant light detection, pathing, and root
    identification.

--- Mosquito/src/mosquito/g3/MidpointPlayerG3.java
+++ Mosquito/src/mosquito/g3/MidpointPlayerG3.java
@@ -20,10 +20,17 @@ import mosquito.sim.Light;
 import mosquito.sim.Player;
 
 public class MidpointPlayerG3 extends Player {
+	
 	private int numLights;
 	private Point2D mainLight;
-	private ArrayList<Point2D> mids;
-	private Set<Line2D> wallCopy;
+	
+	private ArrayList<Point> mids;
+	private ArrayList<Point> ends;
+	private ArrayList<Point> intersects;
+	private double[] slopes;
+	private double[] bVals;
+	private Line2D[] wallCopy;
+	
 	private final double SIG_LEN = 10.0;
 	private Logger log = Logger.getLogger(this.getClass()); // for logging
 	
@@ -32,7 +39,7 @@ public class MidpointPlayerG3 extends Player {
 	 */
 	@Override
 	public String getName() {
-		return "Random Player";
+		return "Critical Points Player";
 	}
 
 	/*
@@ -44,15 +51,51 @@ public class MidpointPlayerG3 extends Player {
 	public void startNewGame(Set<Line2D> walls, int numLights) {
 		log.trace("Starting new game: numLights=" + numLights);
 		
-		wallCopy = walls;
 		
 		if(!walls.isEmpty()){
 			
+			wallCopy = new Line2D[1];
 			
+			wallCopy = (Line2D[]) walls.toArray(wallCopy);
+			
+			initSlopes();
+			initBVals();
+			initMids();
+			initEnds();
+			initIntersections();
+			
+			// this may seem redundant, but it gets awfully modern if I put this
+			// after the if/else block
+			mids.add( new Point( 0, 50 ) );
+			mids.add( new Point( 50, 100 ) );
+			mids.add( new Point( 50, 0 ) );
+			mids.add( new Point( 100, 50 ) );
+			intersects.add( new Point( 0, 0) );
+			intersects.add( new Point( 100, 0) );
+			intersects.add( new Point( 0, 100) );
+			intersects.add( new Point( 100, 100) );
+			
+		}else{
+			
+			// blank board!
+			mids = new ArrayList<Point>();
+			mids.add( new Point( 0, 50 ) );
+			mids.add( new Point( 50, 100 ) );
+			mids.add( new Point( 50, 0 ) );
+			mids.add( new Point( 100, 50 ) );
+			
+			ends = new ArrayList<Point>();
+			
+			intersects = new ArrayList<Point>();
+			intersects.add( new Point( 0, 0) );
+			intersects.add( new Point( 100, 0) );
+			intersects.add( new Point( 0, 100) );
+			intersects.add( new Point( 100, 100) );
 			
 		}
 		
 		this.numLights = numLights;
+		
 	}
 
 	
@@ -62,7 +105,31 @@ public class MidpointPlayerG3 extends Player {
 	 */
 	@Override
 	public Set<Light> getLights() {
+		
 		HashSet<Light> ret = new HashSet<Light>();
+		
+		for( int i=0; i < ends.size(); i++){
+			
+			Light endLight = pointToLight(ends.get(i), 5000, 1, 0);
+			ret.add(endLight);
+			
+		}// end end point population
+		
+		for( int i=0; i < intersects.size(); i++){
+			
+			Light cornerLight = pointToLight(intersects.get(i), 5000, 1, 0);
+			ret.add(cornerLight);
+			
+		}// end end point population
+		
+		for( int i=0; i < mids.size(); i++){
+			
+			Light midLight = pointToLight( (Point) mids.get(i), 5000, 1, 0);
+			ret.add(midLight);
+			
+		}// end end point population
+		
+		System.out.println("There are " + ret.size() + " lights.");
 		
 		return ret;
 	}
@@ -73,23 +140,25 @@ public class MidpointPlayerG3 extends Player {
 	@Override
 	public Collector getCollector() {
 		// place the Collector just to the right of and below the last Light
-		Collector c = new Collector(mainLight.getX()+1,mainLight.getY() +1);
-		log.debug("Positioned Collector at (" + (mainLight.getX()+1) + ", " + (mainLight.getY()+1) + ")");
+		//Collector c = new Collector(mainLight.getX()+1,mainLight.getY() +1);
+
+		Collector c = new Collector(1,1);
+		
+		// log.debug("Positioned Collector at (" + (mainLight.getX()+1) + ", " + (mainLight.getY()+1) + ")");
 		return c;
 	}
 	
-	private ArrayList<Point> generateMids(Set<Line2D> walls){
+	private void initMids(){
 		
-		ArrayList<Point> mids = new ArrayList<Point>();
-		Line2D[] wallList = (Line2D[]) walls.toArray();
+		mids = new ArrayList<Point>();
 		
-		for(int i = 0; i < walls.size(); i++){
+		for(int i = 0; i < wallCopy.length; i++){
 			
-			// checks to see if the wall 
-			if(isSignificant(wallList[i])){
+			// checks to see if the wall is significant
+			if(isSignificant(wallCopy[i])){
 				
-				double newX = ( wallList[i].getX1() + wallList[i].getX2() ) / 2.0;
-				double newY = ( wallList[i].getY1() + wallList[i].getY2() ) / 2.0;
+				double newX = ( wallCopy[i].getX1() + wallCopy[i].getX2() ) / 2.0;
+				double newY = ( wallCopy[i].getY1() + wallCopy[i].getY2() ) / 2.0;
 				
 				Point midpoint = new Point();
 				midpoint.setLocation(newX, newY);
@@ -100,9 +169,25 @@ public class MidpointPlayerG3 extends Player {
 			
 		}// end list population
 		
-		return mids;
-		
 	}
+	
+	private boolean isValidPath(Line2D path){
+		
+		for(int i=0; i < wallCopy.length; i++){
+			
+			// checks to see if light path intersects any walls
+			if( wallCopy[i].intersectsLine( path ) ){
+				
+				return false;
+				
+			}// end blocked check
+			
+		}// end intersection check loop
+		
+		// if you're here, you're in the clear
+		return true;
+		
+	}// end isValidPath method
 	
 	private boolean isSignificant(Line2D wall){
 		
@@ -156,5 +241,111 @@ public class MidpointPlayerG3 extends Player {
 		return mids.get(index);
 		
 	}// end nearestPoint method
+	
+	private void initSlopes(){
+		
+		slopes = new double[wallCopy.length];
+		
+		for(int i=0; i < wallCopy.length; i++){
+			
+			log.trace("Wall " + i + " of " + wallCopy.length);
+			
+			double denom = ( wallCopy[i].getX2() - wallCopy[i].getX1() );
+			
+			// checks for perfectly vertical line
+			if(denom != 0.0){
+				
+				slopes[i] = ( wallCopy[i].getY2() - wallCopy[i].getY1() ) / denom;
+			
+			}else{
+				
+				slopes[i] = Double.NaN;
+			
+			}
+			
+		}// end slope initialization
+		
+	}// end initSlopes method
+	
+	private void initBVals(){
+		
+		bVals = new double[wallCopy.length];
+		
+		for(int i=0; i < bVals.length; i++){
+			
+			if(slopes[i] != Double.NaN){
+				
+				bVals[i] = wallCopy[i].getY1() - ( slopes[i] * wallCopy[i].getX1() ); 
+			
+			}else{
+				
+				bVals[i] = Double.NaN;
+				
+			}
+			
+		}// end bVal population loop
+		
+	}// end initBVals method
+	
+	private void initEnds(){
+		
+		ends = new ArrayList<Point>();
+		
+		for(int i=0; i < wallCopy.length; i++){
+			
+			// only considers significant lines
+			if( isSignificant(wallCopy[i])){
+				
+				// starts are evens, ends are odds
+				ends.add( new Point( (int) wallCopy[i].getX1(), (int) wallCopy[i].getY1() ) );
+				ends.add( new Point( (int) wallCopy[i].getX2(), (int) wallCopy[i].getY2() ) );
+			
+			}
+			
+		}// end endpoint generation
+		
+	}// end initEnds method
+	
+	private void initIntersections(){
+		
+		// initialize the ArrayList
+		intersects = new ArrayList<Point>();
+		
+		for(int i=0; i < wallCopy.length; i++){
+			
+			for(int j=i+1; j < wallCopy.length; j++){
+				
+				if( wallCopy[i].intersectsLine(wallCopy[j]) ){
+					
+					double newX = 0.0;
+					double newY = 0.0;
+					
+					if(bVals[i] != Double.NaN && bVals[j] != Double.NaN){
+						
+						newX = ( bVals[j] - bVals[i] ) / ( slopes[i] - slopes[j] );
+						newY = slopes[i] * newX + bVals[i];
+						
+					}else{
+						
+						// only happens if either of the lines are perfectly vertical
+						if(bVals[i] == Double.NaN){
+							newX = wallCopy[i].getX1();
+							newY = slopes[j] * newX + bVals[j];
+						}else{
+							newX = wallCopy[j].getX1();
+							newY = slopes[i] * newX + bVals[i];
+						}
+						
+					}// end intersection point assignment
+					
+					intersects.add(new Point((int) newX, (int) newY));
+					
+				}// end line intersection point creation
+				
+			}// end line intersection discovery and population
+			
+		}// end exhaustive line intersection check
+		
+	}// end initIntersections method
 
 }
